86692c6d01f616094f93ce182633bc303bc5752b
Nuria <soriano.nuria@gmail.com>
2016-07-07 14:04:01 +0200

    remove duplicate array

--- 06-exercises-arrays/14-removeDuplicate.js
+++ 06-exercises-arrays/14-removeDuplicate.js
@@ -2,16 +2,15 @@ function removeDuplicate(input){
 	var cleanArr = [];
 	function checkelement(elem){
 		var count = 0;
-		input.forEach(function(elemExisting){
-			if(elem === elemExisting){
-				count++;
+		for(i=0;i<input.length;i++){
+			if(input[i]===elem){
+				count++
 			}
-		}	
+		}
 		return count;
 	}
 	input.forEach(function(elem, index, arr){
-		console.log(count);
-		if(count > 1){
+		if(checkelement(elem) <= 1){
 			cleanArr.push(elem);
 		}
 	})
