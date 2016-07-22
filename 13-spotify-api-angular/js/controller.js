05671c42427ca9bca3caf35b152146704a808310
Nuria <soriano.nuria@gmail.com>
2016-07-22 10:37:46 +0200

    separate into controllers

--- 13-spotify-api-angular/js/controller.js
+++ 13-spotify-api-angular/js/controller.js
@@ -1,25 +1,30 @@
 angular.module("spotifyController", ["spotifyServices"])
 	.controller("searchBox", function($scope, dataArtists, dataAlbums, dataTracks) {
-		var urlApi = 'https://api.spotify.com/v1/search?type=artist&query='
 		$scope.artists = "";
 		$scope.albums = "";
-		$scope.albums = "";
+		$scope.tracks = "";
 		$scope.submit = function() {
 			dataArtists.getArtists($scope.searchValue)
 				.then(function( dataFromApi ) {
 					$scope.artists = dataFromApi.data.artists.items;
 				})
 		}
+	})
+	.controller("getAlbumsFromArtist", function($scope, dataAlbums) {
 		$scope.getAlbums = function() {
 			dataAlbums.getAlbums($scope.selectArtists)
 				.then(function( dataFromApi ) {
-					$scope.albums = dataFromApi.data.items;
+					$scope.$parent.albums = dataFromApi.data.items;
 				})
 		}
+	})
+	.controller("getTracksFromAlbum", function($scope, dataTracks) {
 		$scope.getTracks = function() {
 			dataTracks.getTracks($scope.selectAlbums)
 				.then(function( dataFromApi ) {
-					$scope.tracks = dataFromApi.data.items;
+					$scope.$parent.tracks = dataFromApi.data.items;
+
 				})
+
 		}
 	})
